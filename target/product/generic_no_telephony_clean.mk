f90e4bc622a7646ed686192a2b74821d3ad73219
Francesco Montefoschi <francesco.monte@gmail.com>
2016-07-28 15:03:06 +0200

    Restored libRS

--- target/product/generic_no_telephony_clean.mk
+++ target/product/generic_no_telephony_clean.mk
@@ -34,6 +34,14 @@ PRODUCT_PACKAGES += \
     screenrecord
 
 PRODUCT_PACKAGES += \
+    librs_jni \
+    libvideoeditor_jni \
+    libvideoeditor_core \
+    libvideoeditor_osal \
+    libvideoeditor_videofilters \
+    libvideoeditorplayer \
+
+PRODUCT_PACKAGES += \
     audio.primary.default \
     audio_policy.default \
     local_time.default
